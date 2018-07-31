6bbf80ebd312104b9eca5b69e6a5f6acdd7e52a4
halcharger <allen.firth@gmail.com>
2018-07-31 15:29:19 +0200

    adding ExceptionInfo with demystified exception stacktrace for exception logging

---
+++ CodeStream.logDNA/ExceptionInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace CodeStream.logDNA
+{
+    public class ExceptionInfo
+    {
+        public ExceptionInfo(Exception exception, string requestInfo, string requestContent)
+        {
+            Exception = exception;
+            RequestInfo = requestInfo;
+            RequestContent = requestContent;
+            DemystifiedException = exception.ToStringDemystified();
+        }
+
+        public Exception Exception { get; set; }
+        public string RequestInfo { get; set; }
+        public string RequestContent { get; set; }
+        public string DemystifiedException { get; set; }
+    }
+}
